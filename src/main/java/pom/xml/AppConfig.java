70efff38c2cb77eae5c206e30615b0bd0c9f35bc
yrnThiago <thiagodias_123@hotmail.com>
2024-04-28 23:44:42 -0300

    feat: add config files

---
+++ src/main/java/pom/xml/AppConfig.java
@@ -0,0 +1,68 @@
+package pom.xml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import pom.xml.utils.ReadFile;
+
+public class AppConfig {
+    private static final Properties prop = new Properties();
+    private static final String CONFIG_FILE = "src\\main\\resources\\app.config";
+
+    static {
+        try (FileInputStream fis = new FileInputStream(CONFIG_FILE)) {
+            prop.load(fis);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+
+    public static String getDatabaseURL() {
+        return prop.getProperty("DB.URL");
+    }
+
+    public static String getDatabaseUsername() {
+        return prop.getProperty("DB.USERNAME");
+    }
+
+    public static String getDatabasePassword() {
+        return prop.getProperty("DB.PASSWORD");
+    }
+
+    public static String getOllamaHost() {
+        return prop.getProperty("OLLAMA.HOST");
+    }
+    
+    public static String getOllamaPort() {
+        return prop.getProperty("OLLAMA.PORT");
+    }
+
+    public static String getOllamaUrl() {
+        return "http://" + getOllamaHost() + ":" + getOllamaPort(); 
+    }
+
+    public static String getDbSchemaFile() {
+        return prop.getProperty("DB.SCHEMA"); 
+    }
+
+    public static String getDbSchema() {
+        String dbSchema = "";
+        try {
+            dbSchema = ReadFile.convertTextFileToString(getDbSchemaFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return dbSchema;
+    }
+    
+}
+
